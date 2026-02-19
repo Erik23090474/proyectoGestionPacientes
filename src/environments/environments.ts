4978e38b36a8896aa5b4c48087c2a0f57605eb4d
Brandon Diaz <utm23090474@utma.edu.mx>
2026-02-19 14:48:00 -0600

    Finalizados pasos 6 al 10: Auth, Login, Pacientes y Modelo

---
+++ src/environments/environments.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBueSHSW170k3BlSi0GR3WHDrt34lMjGBM",
+    authDomain: "gestion-pacientes-d6306.firebaseapp.com",
+    projectId: "gestion-pacientes-d6306",
+    storageBucket: "gestion-pacientes-d6306.firebasestorage.app",
+    messagingSenderId: "375288246658",
+    appId: "1:375288246658:web:3e60b43d6cf0756c105f9b"
+  }
+};
